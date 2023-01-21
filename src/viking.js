6b1cbe5204185e0c6b0cb02b05da8bebfeff03e3
Ivan <Ivandgea@gmail.com>
2023-01-21 18:59:44 +0100

    Lab solved

--- src/viking.js
+++ src/viking.js
@@ -65,28 +65,42 @@ class War {
     }
 
     vikingAttack() {
-        let randomSaxon = Math.floor(Math.random()) * (this.saxonArmy.length);
-        let randomViking = Math.floor(Math.random()) * (this.vikingArmy.length);
+
+        let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+        let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+        if (this.saxonArmy.length === 0 || this.vikingArmy.length === 0) return;
 
         let resultadoAttack = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
 
-        this.saxonArmy = this.saxonArmy.filter(sax => sax.health > 0);
-
-        //hacerloen if si la vida es 0 lo sacamos de la matriz
-
-
-
-        return resultadoAttack
-
-
-
+        if (this.saxonArmy[randomSaxon].health <= 0) {
+            this.saxonArmy.splice(randomSaxon, 1);
+        }
+        return resultadoAttack;
     }
 
     saxonAttack() {
 
         let randomSaxon = Math.floor(Math.random()) * (this.saxonArmy.length);
-        let randomVikings = Math.floor(Math.random()) * (this.vikingArmy.length);
+        let randomViking = Math.floor(Math.random()) * (this.vikingArmy.length);
 
+        let resultadoAttack = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+
+        if (this.vikingArmy[randomViking].health === 0) {
+            this.vikingArmy.splice(randomViking, 1);
+        }
+
+        return resultadoAttack;
+    }
+
+    showStatus() {
+        if (this.vikingArmy.length === 0) {
+            return "Saxons have fought for their lives and survived another day...";
+        } else if (this.saxonArmy.length === 0) {
+            return "Vikings have won the war of the century!";
+        } else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
     }
 }
-
